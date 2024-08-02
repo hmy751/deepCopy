d3aa5226da81d2e747916e122c5515b24a6ea76d
hmy751 <hmy751@gmail.com>
2024-08-02 15:17:06 +0900

    chroe: jest.config.js testMatch 셋팅 수정

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   testMatch: [
     "<rootDir>/**/*.test.(js|jsx|ts|tsx)",
-    "<rootDir>/(tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx))",
+    "<rootDir>/**/*.spec.(js|jsx|ts|tsx)",
   ],
   transformIgnorePatterns: ["<rootDir>/node_modules/"],
 };
