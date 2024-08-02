8bdb7b38524301fb042d9b3faaf2366da3966964
hmy751 <hmy751@gmail.com>
2024-08-02 16:57:36 +0900

    test: 객체 데이터, 중첩된 객체 데이터의 복사 기능 테스트 추가

--- deepCopy/index.spec.js
+++ deepCopy/index.spec.js
@@ -18,6 +18,34 @@ describe("deepCopy 함수 테스트", () => {
     expect(JSON.stringify(copiedObj) === JSON.stringify(obj)).toBe(true);
   });
 
+  it("객체 데이터를 인자로 넘길 때, 중첩된 객체일 경우 중첩된 프로퍼티까지 새로운 데이터를 바라보도록 해야한다.", () => {
+    const obj = {
+      a: 1,
+      b: {
+        x: 1,
+        y: { c: 2, d: 3 },
+      },
+    };
+    const obj2 = obj;
+    const copiedObj = deepCopy(obj);
+
+    copiedObj.a = 2;
+    copiedObj.b.x = 10;
+    copiedObj.b.y.d = 20;
+
+    expect(obj.a === 1).toBe(true);
+    expect(obj.b.x === 1).toBe(true);
+    expect(obj.b.y.d === 3).toBe(true);
+
+    obj2.a = 2;
+    obj2.b.x = 10;
+    obj2.b.y.d = 20;
+
+    expect(obj.a === 2).toBe(true);
+    expect(obj.b.x === 10).toBe(true);
+    expect(obj.b.y.d === 20).toBe(true);
+  });
+
   it("배열 데이터를 인자로 넘겨 반환된 배열은 원본과 달라야 한다.", () => {
     const c = [1, 2, 3];
     const d = c;
